5588cd71c6d3b13ac1f184399f09baa21281dba8
Eduardo Xuya <ozcar2294@gmail.com>
2025-05-18 10:35:15 -0600

    fix(reportes): fix service file

--- src/modules/reporte/services/reporte.service.ts
+++ src/modules/reporte/services/reporte.service.ts
@@ -9,17 +9,14 @@ export class ReporteService {
     }
 
     async getTopProductos(limite: number = 10, porMonto: boolean = false): Promise<ITopProducto[]> {
-        // Eliminamos la validación de límite ya que se realiza en el middleware
         return await this.repository.getTopProductos(limite, porMonto);
     }
 
     async getTopClientes(limite: number = 5, porTransacciones: boolean = true): Promise<ITopCliente[]> {
-        // Eliminamos la validación de límite ya que se realiza en el middleware
         return await this.repository.getTopClientes(limite, porTransacciones);
     }
 
     async getVentasPorPeriodo(fechaInicio: Date, fechaFin: Date): Promise<any> {
-        // Eliminamos las validaciones de fecha ya que se realizan en el middleware
         return await this.repository.getVentasPorPeriodo(fechaInicio, fechaFin);
     }
 }
